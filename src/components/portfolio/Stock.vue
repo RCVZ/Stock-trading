2ef4e9c93f4cd34b1ae5bd329da06918528b192b
RCVZ <robinvz17@gmail.com>
2019-02-24 23:55:55 +0100

    init Comp

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Stock.........</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-succes">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          NAME
+          <small>(Price: PRICE)</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Aantal"
+          >
+        </div>
+        <div class="pull-right">
+          <button class="btn btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
