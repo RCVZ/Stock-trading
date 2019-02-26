80dc2539e3785b82ccde5a8d2740d6e78fa5218d
RCVZ <robinvz17@gmail.com>
2019-02-26 21:49:00 +0100

    added vue-resource

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
+import VueResource from 'vue-resource';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueResource);
+Vue.http.options.root = 'https://stock-trading-vue.firebaseio.com/';
 
 new Vue({
   router,
